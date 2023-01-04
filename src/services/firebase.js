9f2300295aa06666b602cbbcd72402cfc9242e6c
Pedro de Andrade Reis <61993076+queendeveloperbr@users.noreply.github.com>
2023-01-04 20:35:26 -0300

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,13 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  /*apiKey: "AIzaSyBoCCslyNaSQPuR16NOqoxktxtIgQE411E",
-  authDomain: "mechanic-workshop-service.firebaseapp.com",
-  projectId: "mechanic-workshop-service",
-  storageBucket: "mechanic-workshop-service.appspot.com",
-  messagingSenderId: "807101110177",
-  appId: "1:807101110177:web:39c46e9641c5c6653c5de0",*/
-  databaseURL: "https://mechanic-workshop-service-default-rtdb.firebaseio.com"
+  databaseURL: "your-url-paunocu"
 };
 
 // Initialize Firebase
